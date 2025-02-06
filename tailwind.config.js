fe53d4047051e27dbf120c20ce36b19444ac9f51
Cordelia013 <cordeliaguims@yahoo.com>
2025-02-06 16:54:35 +0100

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+      './src/**/*.{html,js,jsx,ts,tsx,vue}',
+    './public/**/*.html'
+  ],
+  theme: {
+   
+    fontFamily: {
+      futura: ['futura-pt', 'sans-serif'],
+     
+    },
+    extend: {
+      
+    
+    },
+
+    plugins: [],
+  }
+
+}
