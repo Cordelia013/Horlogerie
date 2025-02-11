97354777d387f86f32dcb2aa432a9fb94b371f99
Cordelia013 <cordeliaguims@yahoo.com>
2025-02-11 12:59:34 +0100

    prob build, suite manque decla isLoading-ok

--- src/section/HeroSection.tsx
+++ src/section/HeroSection.tsx
@@ -2,8 +2,8 @@ import React, { useState, useRef } from "react";
 import Menu from "../components/Menu";
 
 const HeroSection: React.FC = () => {
-  const [isPlaying, setIsPlaying] = useState(true); // État pour contrôler la lecture de la vidéo
-  const [isLoading, setIsLoading] = useState(true); // État pour contrôler le chargement de la vidéo
+  const [isPlaying, setIsPlaying] = useState(true); // Contrôle de la lecture de la vidéo
+  const [isLoading, setIsLoading] = useState(true); // Contrôle du chargement de la vidéo
   const videoRef = useRef<HTMLVideoElement | null>(null); // Référence à la vidéo
 
   const handleLoadedData = () => {
@@ -32,36 +32,40 @@ const HeroSection: React.FC = () => {
   };
 
   return (
-    <>
-      <div className="relative w-full h-screen flex items-end justify-center bg-cover bg-center">
-        <video
-          ref={videoRef}
-          style={{ zIndex: 0 }}
-          className="absolute inset-0 w-full h-full object-cover"
-          src="/assets/HUBLOT-BIG_BANG_UNICO_NESPRESSO_ORIGIN.mp4"
-          onClick={toggleVideo}
-          // muted
-          playsInline
-          autoPlay
-        />
-
-        <button
-          onClick={togglePlayPause}
-          className="absolute bottom-4 left-4 bg-white bg-opacity-80 text-secondary p-2 rounded z-10 shadow-md"
-        >
-          {isPlaying ? "Mettre en pause" : "Jouer"}
-        </button>
-
-        <div className="container mx-auto flex items-center justify-center">
-          {" "}
-          {/* Ajout de la classe container */}
-          <div className="absolute inset-0 ">
-            <div className="border border-black mx-[135px] mt-[136px] mb-[137px] w-[calc(100%-270px)] h-[calc(100%-273px)]"></div>
-          </div>
-          <Menu />
+    <div className="relative w-full h-screen flex items-end justify-center bg-cover bg-center">
+      {/* Affiche un indicateur tant que la vidéo est en cours de chargement */}
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center z-20 bg-white bg-opacity-75">
+          <span>Chargement...</span>
         </div>
+      )}
+
+      <video
+        ref={videoRef}
+        style={{ zIndex: 0 }}
+        className="absolute inset-0 w-full h-full object-cover"
+        src="/assets/HUBLOT-BIG_BANG_UNICO_NESPRESSO_ORIGIN.mp4"
+        onClick={toggleVideo}
+        onLoadedData={handleLoadedData} // Utilise isLoading en mettant à jour l'état
+        muted
+        playsInline
+        autoPlay
+      />
+
+      <button
+        onClick={togglePlayPause}
+        className="absolute bottom-4 left-4 bg-white bg-opacity-80 text-secondary p-2 rounded z-10 shadow-md"
+      >
+        {isPlaying ? "Mettre en pause" : "Jouer"}
+      </button>
+
+      <div className="container mx-auto flex items-center justify-center">
+        <div className="absolute inset-0 pointer-events-none">
+          <div className="border border-black mx-[135px] mt-[136px] mb-[137px] w-[calc(100%-270px)] h-[calc(100%-273px)]"></div>
+        </div>
+        <Menu />
       </div>
-    </>
+    </div>
   );
 };
 
