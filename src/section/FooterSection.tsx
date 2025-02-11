60fcc569ba07e144d04d3c5d41e1a6e331375c9d
Cordelia013 <cordeliaguims@yahoo.com>
2025-02-11 20:42:19 +0100

    rectif footer

--- src/section/FooterSection.tsx
+++ src/section/FooterSection.tsx
@@ -2,7 +2,7 @@
 
 function FooterSection() {
   return (
-    <div className="w-full h-[26rem] relative bg-primary flex flex-col text-gray font-Akkordeon  z-20">
+    <div className="w-full h-[29em] relative bg-primary flex flex-col text-gray font-Akkordeon  z-20">
       <div className="flex justify-between flex-row mx-4 pt-10 mt-[117px]  font-montreal bottom-0">
         <div className="flex text-base gap-8">
           <div className="">ACCEUIL </div>
