eddfd483a7a51af0af94558c2a7b08cc10839ceb
Cordelia013 <cordeliaguims@yahoo.com>
2025-03-23 22:54:31 +0100

    responsivité améliorer  pour présenttion section

--- src/section/PresentationSection.tsx
+++ src/section/PresentationSection.tsx
@@ -3,24 +3,24 @@ import BlurBackground from "../components/BlurBackground";
 function PresentationSection() {
   return (
     <div
-      className="relative w-screen h-screen bg-primary text-gray font-Akkordeon font-medium tracking-[10px] text-[200px] xl:text-[270px] z-20 xl:z-30 pb-[10%]"
+      className="relative w-screen  text-gray font-Akkordeon font-medium tracking-[10px] text-[10vw]  z-20 xl:z-30 pb-[10%]"
       id="presentation-section"
     >
-      <div className="flex justify-end mr-[135px] xl:relative xl:top-14 tracking-[10px]">NEW</div>
-      <div className="ml-3 xl:ml-20 xl:relative xl:bottom-12">COLLABORATION</div>
+      <div className="flex justify-end mr-[5vw]  relative top-10 tracking-[10px] text-[15vw] ">NEW</div>
+      <div className="relative bottom-[19vh] text-[15vw] ">COLLABORATION</div>
       <div className="  flex justify-center items-center  relative bottom-96 ">
         <BlurBackground />
         <img
           src="/assets/watch 1.png"
           alt="whatch"
-          className="h-screen  xl:relative xl:bottom-[250px]"
+          className="relative bottom-[35vh] w-[40vw]"
         />
       </div>
 
-      <div className="flex justify-end absolute top-[28rem] xl:top-[38rem]  xl:mr-[20rem] right-0">
+      <div className="flex justify-end absolute  top-[75vh] text-[15vw] right-[5vw] ">
         NESPRESSO
       </div>
-      <div className="flex justify-start absolute top-[40rem] xl:top-[55rem] left-0 xl:left-28 ml-[131px]">
+      <div className="flex justify-start absolute top-[100vh] text-[15vw] left-[5vw] ">
         HUBLOT
       </div>
     </div>
